9296511671f70d05aa52849a9492179c69077400
shubham-pawar9 <developer.shubhampawar@gmail.com>
2023-10-04 11:11:12 +0530

    start work on next js app

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import './globals.css'
+export default function Home() {
+  return (
+    <>
+    <h2 className="heading2">My First Next JS Application</h2>
+    </>
+  )
+}
